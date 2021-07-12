6fac8dc299d6b463754ac2c4bb6227af3d735be6
navjeetgill <navjeetgill240297@gmail.com>
2021-07-12 21:04:34 +0530

    frth

--- detail/createin.tf
+++ detail/createin.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "MyFiirstInstnace" { 
-  ami           = "ami-0d563aeddd4be7fff"
+  ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
 
   tags = {
--- detail/variable.tf
+++ detail/variable.tf
@@ -10,3 +10,14 @@ variable "Security_Group"{
     type = "list"
     default =["sg-24076","sg-90890", "sg-456789"]
 }
+
+variable "AMIS" {
+    type = map
+    default = {
+        us-east-1 = "ami-0ee02acd56a52998e"
+        us-east-2 = "ami-0d563aeddd4be7fff" 
+        us-west-2 = "ami-0dd273d94ed0540c0"  
+        us-west-1 = "ami-008464c6813f3241e"
+    }
+  
+}
