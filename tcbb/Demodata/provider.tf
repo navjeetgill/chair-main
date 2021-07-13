e29634028d4e30373a967444da32b38eec00703e
navjeetgill <navjeetgill240297@gmail.com>
2021-07-14 00:09:58 +0530

    gud

--- tcbb/Demodata/provider.tf
+++ tcbb/Demodata/provider.tf
@@ -2,4 +2,15 @@ provider "aws" {
   access_key = "${var.AWS_ACCESS_KEY}"
   secret_key = "${var.AWS_SECRET_KEY}"
   region     = "${var.AWS_REGION}"
+  profile = "awsconfig"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.49.0"
+    }
+  }
+}
+
