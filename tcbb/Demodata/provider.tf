45e0dfd5f4502b4ac76a306ae04ad5dc34c040f3
navjeetgill <navjeetgill240297@gmail.com>
2021-07-14 00:16:25 +0530

    hh

--- tcbb/Demodata/provider.tf
+++ tcbb/Demodata/provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   access_key = "${var.AWS_ACCESS_KEY}"
   secret_key = "${var.AWS_SECRET_KEY}"
   region     = "${var.AWS_REGION}"
-  profile = "awsconfig"
+  profile = "provider"
 }
 
 terraform {
