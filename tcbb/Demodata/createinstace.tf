8afa78a9ac54dfd16b8f73df2c7418adece8ec46
navjeetgill <navjeetgill240297@gmail.com>
2021-07-13 17:42:10 +0530

    dd

---
+++ tcbb/Demodata/createinstace.tf
@@ -0,0 +1,13 @@
+
+
+resource "aws_instance" "MyFiirstInstnace" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "custom_instance"
+  }
+
+}
+     
+  
