5a68c6beb2e3da703d8a6e0095540e76532547b8
navjeetgill <navjeetgill240297@gmail.com>
2021-07-14 13:05:14 +0530

    avl

--- tcbb/demodatasource/createinstace.tf
+++ tcbb/demodatasource/createinstace.tf
@@ -1,12 +1,13 @@
-
+data "aws_availablility_zones" {}
     
 
-resource "aws_instance" "MyFirstInstnace" {
+resource "aws_instance" "MyFiirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  
+  availablility_zone = data.aws_availablility_zones.available.names[1]
+
   tags = {
     Name = "custom_instance"
   }
-
+}
   
