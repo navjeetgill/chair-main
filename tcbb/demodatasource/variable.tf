aa8ca4a5abd69eff4156d2f6bbcd80b2efa279a8
navjeetgill <navjeetgill240297@gmail.com>
2021-07-15 10:55:43 +0530

    security

--- tcbb/demodatasource/variable.tf
+++ tcbb/demodatasource/variable.tf
@@ -14,8 +14,7 @@ variable "Security_Group"{
 variable "AMIS" {
     type = map
     default = {
-        us-east-1 = "ami-0ee02acd56a52998e"
-        us-east-2 = "ami-0d563aeddd4be7fff" 
+           
         us-west-2 = "ami-0dd273d94ed0540c0"  
         us-west-1 = "ami-008464c6813f3241e"
     }
