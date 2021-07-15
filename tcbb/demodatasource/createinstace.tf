e42d0b5e26e38694486a0328b22467435a14eed4
navjeetgill <navjeetgill240297@gmail.com>
2021-07-15 16:43:07 +0530

    out

--- tcbb/demodatasource/createinstace.tf
+++ tcbb/demodatasource/createinstace.tf
@@ -2,11 +2,26 @@
 resource "aws_instance" "MyFirstInstnace" {
   ami           = data.aws_ami_ids.example.id
   instance_type = "t2.micro"
+
+
+  provisioner "local-exec" {
+    command = "echo aws_instance.MyFiirstInstnace.private_ip >> my_private_ips.txt"
+
+  }
   
   tags = {
     Name = "custom_instance"
   }
 
-} 
-
  
+
+output "public_ip" {
+  value = aws_instance.MyFiirstInstnace.public_ip
+  }
+
+
+}
+
+
+
+
